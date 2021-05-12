8ab5d2885281e859a1c83ebeb2bd0475f58c7981
junseongwoo <wnstjd4852@gmail.com>
2021-05-12 14:24:29 +0900

    0512 자료구조 커밋

---
+++ Chap2/2-5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i, length = 0;
+	char str[50];
+	printf("\n문자열을 입력하세요 : ");
+	gets(str);
+	printf("\n입력된 문자열은 \n \"");
+	for (i = 0; str[i]; i++)
+	{
+		printf("%c", str[i]);
+		length += 1;
+	}
+	printf("\" \n입니다.");
+	printf("\n\n입력된 문자열의 길이 = %d \n", length);
+
+	getchar();
+	return 0;
+}
